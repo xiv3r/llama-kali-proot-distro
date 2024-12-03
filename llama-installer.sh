f4d4852f648e4f770128b7b5a8b396e3c802ac9b
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 13:33:50 +0800

    Update llama-installer.sh

--- llama-installer.sh
+++ llama-installer.sh
@@ -4,7 +4,7 @@ apt update
 ###
 apt full-upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y
 ##
-apt install curl -y
+apt install curl golang -y
 ###
 echo "ollama serve > /dev/null 2>&1 &" >> /etc/bash.bashrc
 ###
